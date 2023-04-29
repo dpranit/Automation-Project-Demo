6214ca35856b55f603e8429eba7ddff902923301
Pranit <92619668+dpranit@users.noreply.github.com>
2023-04-29 18:07:41 +0530

    changes

--- src/main/java/utils/BaseClass.java
+++ src/main/java/utils/BaseClass.java
@@ -26,9 +26,9 @@ public class BaseClass {
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		WebElement Email= driver.findElement(By.xpath("//input[@placeholder='Enter email address']"));
-		Email.sendKeys("dpranit07@gmail.com");
+		Email.sendKeys("@gmail.com");
 		WebElement Pass = driver.findElement(By.xpath("//input[@placeholder='Enter password']"));
-		Pass.sendKeys("Aa123456789");
+		Pass.sendKeys("");
 		WebElement LoginBtn = driver.findElement(By.xpath("//button[text()='Login now']"));
 		LoginBtn.click();
 		}
